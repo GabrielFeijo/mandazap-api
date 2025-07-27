8f889a3822e04afdec620c61c300d29484386872
Gabriel Feijó <92588394+GabrielFeijo@users.noreply.github.com>
2025-07-27 00:37:43 -0300

    feat: initialize application structure with AppController, AppService, AppModule, and e2e tests

---
+++ src/app.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { WhatsAppModule } from './whatsapp/whatsapp.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    PrismaModule,
+    AuthModule,
+    UsersModule,
+    WhatsAppModule,
+  ],
+})
+export class AppModule {}
---
+++ src/main.ts
@@ -0,0 +1,22 @@
+import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { AppModule } from './app.module';
+import { IoAdapter } from '@nestjs/platform-socket.io';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  app.useWebSocketAdapter(new IoAdapter(app));
+  app.enableCors();
+
+  await app.listen(3333);
+}
+void bootstrap();
